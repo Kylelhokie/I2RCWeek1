eefad75f416558d450a9a4dd75aaa8679f593f01
Kylelhokie <145638764+Kylelhokie@users.noreply.github.com>
2023-10-04 22:00:07 -0700

    Comments on TankDrive

--- src/main/java/frc/robot/commands/TankDrive.java
+++ src/main/java/frc/robot/commands/TankDrive.java
@@ -8,14 +8,14 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveTrain;
 
-public class TankDrive extends CommandBase {
-  public DriveTrain dt;
-  public Joystick joy;
+public class TankDrive extends CommandBase { 
+  public DriveTrain dt; //creates a new DriveTrain variable
+  public Joystick joy; //creates a new Joystick variable
 
   /** Creates a new TankDrive. */
-  public TankDrive(DriveTrain dt, Joystick j) {
-    this.dt = dt;
-    this.joy = j;
+  public TankDrive(DriveTrain dt, Joystick j) { //moves DriveTrain dt and Joystick j to the TankDrive class
+    this.dt = dt; //Sets variable dt = dt
+    this.joy = j; //Sets variable joy = j
 
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(dt);
@@ -24,28 +24,28 @@ public class TankDrive extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    dt.tankDrive(0.0, 0.0);
+    dt.tankDrive(0.0, 0.0); 
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double leftPowerRaw = joy.getRawAxis(1);
+    double leftPowerRaw = joy.getRawAxis(1); //Gets joystick axis 1
 
-    double rightPowerRaw = joy.getRawAxis(5);
+    double rightPowerRaw = joy.getRawAxis(5); //Gets joystick axis 5
 
-    dt.tankDrive(leftPowerRaw*-0.7, rightPowerRaw*-0.7);
+    dt.tankDrive(leftPowerRaw*0.7, rightPowerRaw*0.7); //Converts the axis into a speed by scaling by 0.7
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    dt.tankDrive(0.0, 0.0);
+    dt.tankDrive(0.0, 0.0); //sets the speed to 0.0 since the command is ended or interrupted 
   }
 
   // Returns true when the command should end.
   @Override
-  public boolean isFinished() {
+  public boolean isFinished() { 
     return false;
   }
 }
